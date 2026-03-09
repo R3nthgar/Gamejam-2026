bdb8d6005cb3954d1dda46b5bd3dbbe6098e1638
R3nthgar <110404945+R3nthgar@users.noreply.github.com>
2026-03-09 16:07:00 -0400

    Lava improvements

--- scripts/lava.gd
+++ scripts/lava.gd
@@ -12,5 +12,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#direction+=0.01
-	temp+=Vector2(delta,0).rotated(direction)
+	temp+=Vector2(delta*16*0.2,0).rotated(direction)
 	material.set_shader_parameter("texture_pos", temp)
